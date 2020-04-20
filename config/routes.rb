e0151b9f0ba82f38522b589c02fe88f85aac5c56
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-04-20 16:05:29 +0900

    [Add] ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
+
+  devise_for :users
+  resources :users, only:[:index, :show]
+  resources :conversations do
+    resources :messages
+  end
 end

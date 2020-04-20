2b579dece7cb078c1143813d191fa53b911f5ff0
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-04-20 17:08:34 +0900

    [Add] googleアカウントによるログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
     root :to => "devise/sessions#new"
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :users, only:[:index, :show]
   resources :conversations do
     resources :messages

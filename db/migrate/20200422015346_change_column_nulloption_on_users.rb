c98f4649519f5956788219cd98b0fed322e67ed0
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-04-22 10:55:04 +0900

    [Change] userテーブルのimageカラムのnullを許可する

---
+++ db/migrate/20200422015346_change_column_nulloption_on_users.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNulloptionOnUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :users, :image, null: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_110131) do
+ActiveRecord::Schema.define(version: 2020_04_22_015346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_110131) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.text "note", default: "", null: false
-    t.text "image", default: "", null: false
+    t.text "image", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

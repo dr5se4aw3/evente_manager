83e56fd09c2423703972888a30a7c2e4fa1e3912
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-05-02 18:43:11 +0900

    [Add] イベント一覧画面にて、主催イベントあるいは参加済みイベントには目印を表示する

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,5 +11,5 @@ class Event < ApplicationRecord
   # アソシエーション
   belongs_to :user
   has_many :event_participants , dependent: :destroy
-  has_many :participate_users, through: :favorites, source: :user
+  has_many :participate_users, through: :event_participants, source: :user
 end
